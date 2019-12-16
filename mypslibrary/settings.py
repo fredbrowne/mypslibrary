9516c69f73403625444137632e8ae87b103521a7
Fred Setra <fredericosetra@yahoo.com.br>
2019-12-15 21:53:47 -0300

    Database fix

--- mypslibrary/settings.py
+++ mypslibrary/settings.py
@@ -127,17 +127,3 @@ LOGOUT_REDIRECT_URL = '/'
 
 # Heroku deploy configuration
 ALLOWED_HOSTS = ['*']
-
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-
-DATABASES = { 'default' : dj_database_url.config()}
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# try to load local_settings.py if it exists
-try:
-  from local_settings import *
-except Exception as e:
-  pass
